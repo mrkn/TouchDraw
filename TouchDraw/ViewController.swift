07c8b7d981586c1879a077ad55966f7aaba31bb7
Kenta Murata <mrkn@mrkn.jp>
2016-01-02 22:08:08 +0900

    Initial Commit

---
+++ TouchDraw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchDraw
+//
+//  Created by Kenta Murata on 1/2/16.
+//  Copyright © 2016 Kenta Murata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
